30ce654d6d9aa52309a3df504fd81ac1e91274c9
yearsh7052 <yearsh7052@gmail.com>
2024-11-27 16:08:52 +0530

    Update Footer.js

--- src/Screen Components/Footer.js
+++ src/Screen Components/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Footer() {
 
   const navigate = useNavigate()
+
+  const [phnScreen, setPhoneScreen]=useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 450) {
+        setPhoneScreen(true)
+      }else {
+        setPhoneScreen(false)
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className='bg-black w-full flex flex-col items-center justify-center '>
+    <div className='bg-black w-full flex flex-col items-center justify-center font-cormorant-garamond '>
     {/* Book your session section  */}
     <div className="w-full overflow-hidden flex flex-col items-center justify-center p-[100px] box-border bg-[url('/public/desktop--8@3x.png')] bg-cover bg-no-repeat bg-[top] text-white   tab:p-[50px] phn:p-[20px] ">
     <div className="flex flex-col items-center justify-start gap-10 tab:gap-5">
@@ -18,20 +38,27 @@ function Footer() {
     </div>
     </div>
     <div className="flex flex-row items-center justify-center gap-10 text-13xl tab:gap-5   lg:text-8xl tab:text-xl sphn:text-[17px] sphn:gap-2">
-    <div onClick={()=>alert('Email will be connected tomorrow after discussion')} className="relative tracking-[-0.04em] font-medium cursor-pointer">
-    xyzxyz@gmail.com
+
+    <a className="no-underline  text-white"
+    href='https://mail.google.com/mail/?view=cm&fs=1&to=weddingsbyrishabh@gmail.com' target='_blank'>
+    <div className="relative tracking-[-0.04em] font-medium cursor-pointer">
+    weddingsbyrishabh@gmail.com
     </div>
+    </a>
+
     <div className="w-0.5 relative rounded-81xl bg-white h-[50px] overflow-hidden shrink-0" />
-    <div onClick={()=>alert('Watsap No. will be added tomorrow after discussion')} className="relative tracking-[-0.04em] font-medium cursor-pointer">
+    <a className="no-underline  text-white" href={!phnScreen ? 'https://web.whatsapp.com/send?phone=9315627762' : "https://wa.me/9315627762"} target='_blank'>
+    <div className="relative tracking-[-0.04em] font-medium cursor-pointer">
     Chat on WhatsApp
     </div>
+    </a>
     </div>
     </div>
     </div>
 
     {/* Logo and other section */}
-    <div className={` self-stretch bg-[#F6F5F4] overflow-hidden flex flex-col items-center justify-start p-[50px] gap-5 text-left text-8xl text-gray-600 font-optima-nova-lt-pro 
-     phn:p-[20px] tab:text-xl phn:text-sm `}
+    <div className={` self-stretch bg-[#F6F5F4] overflow-hidden flex flex-col items-center justify-start p-[50px] gap-5 text-left text-8xl text-gray-600 font-cormorant-garamond
+     phn:p-[20px] tab:text-xl phn:text-mid `}
     >
       <div className="w-auto relative h-[800.2px] hidden" />
 
